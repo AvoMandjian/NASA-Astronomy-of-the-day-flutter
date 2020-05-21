a40de8429d85771521d2d79806724830343c001b
Avo Mandjian <avomandjian@gmail.com>
2020-05-21 22:20:08 +0300

    after adding http request

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'dart:math';
 
 class MyApp extends StatefulWidget {
   @override
@@ -7,16 +10,41 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   int i = 0;
+  Random range = Random();
+
+  String url = 'https://jsonplaceholder.typicode.com/albums/1/photos';
+  var data;
+  var imgURL = '';
+
+  Future getData() async {
+    http.Response response = await http.get(url);
+    setState(() {
+      data = json.decode(response.body);
+      imgURL = data[range.nextInt(49)]['url'];
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('$i images'),
+          child: Image.network(
+            '$imgURL',
+          ),
         ),
         appBar: AppBar(
           title: Text('Let\'s see some Images'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              i++;
+              getData();
+              print(imgURL);
+            });
+          },
+          child: Icon(Icons.add_a_photo),
         ),
       ),
     );
